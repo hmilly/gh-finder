6b5c922621939b5ad886bea5bda8ca2d5eabd6cf
hmilly <milstowers@gmail.com>
2022-08-07 16:30:23 +0100

    feat: page data added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-
-
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1 className="text-6xl">Welcome </h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
